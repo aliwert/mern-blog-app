fa923bb549298459c858189386ba275eed239b81
aliwert <154356044+aliwert@users.noreply.github.com>
2024-08-25 20:31:08 +0300

    feat: footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,14 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <div className="mt-10 w-full bg-slate-900 md:px-[500px] flex justify-between text-sm md:text-md py-8">
+      <div className="flex flex-col text-purple-300">
+        <span>@{year} All rights reserved</span>
+        <span>Privacy Policy</span>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,9 @@ const Home = () => {
   return (
     <div className="px-6 md:px-[12rem]">
       <HomePost />
+      <HomePost />
+      <HomePost />
+      <HomePost />
     </div>
   );
 };
