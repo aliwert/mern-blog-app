6c70b9a410ac9ba26ecf572d0af35b6d7b3f6175
aliwert <154356044+aliwert@users.noreply.github.com>
2024-08-26 18:31:17 +0300

    feat: create category

--- frontend/src/pages/CreatePost.jsx
+++ frontend/src/pages/CreatePost.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import { RxCross2 } from "react-icons/rx";
 
 const CreatePost = () => {
+  const [category, setCategory] = useState("");
+  const [categories, setCategories] = useState([]);
+  const addCategory = () => {
+    let updatedCategories = [...categories];
+    updatedCategories.push(category);
+    setCategory("");
+    setCategories(updatedCategories);
+  };
+  const deleteCategory = () => {};
+
   return (
     <div>
       <Navbar />
@@ -23,35 +34,45 @@ const CreatePost = () => {
           <div className="flex flex-col ">
             <div className="flex items-center space-x-4 md:space-x-8 ">
               <input
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
                 type="text"
                 className="bg-transparent border rounded-lg px-3 py-2 outline-none"
                 placeholder="Enter Post Category"
               />
-              <div className="bg-slate-600 text-white px-3 py-2 font-bold cursor-pointer transition-all rounded-md shadow-xl sm:w-auto bg-gradient-to-r from-blue-600 to-blue-500 hover:bg-gradient-to-b dark:shadow-blue-900 shadow-blue-200 hover:shadow-2xl hover:shadow-blue-400 hover:-tranneutral-y-px">
+              <div
+                onClick={addCategory}
+                className="bg-slate-600 text-white px-3 py-2 font-bold cursor-pointer transition-all rounded-md shadow-xl sm:w-auto bg-gradient-to-r from-blue-600 to-blue-500 hover:bg-gradient-to-b dark:shadow-blue-900 shadow-blue-200 hover:shadow-2xl hover:shadow-blue-400 hover:-tranneutral-y-px"
+              >
                 Add
               </div>
             </div>
             <div className="flex px-4 mt-3">
-              <div className="flex justify-center items-center space-x-3 mr-3 bg-sky-600 px-2 py-1 rounded-md mt-4 w-28">
-                <p>Tech</p>
-                <p className="text-red-700rounded-full cursor-pointer p-1">
-                  <RxCross2 />
-                </p>
-              </div>
-              <div className="flex justify-center items-center space-x-3 mr-3 bg-sky-600 px-2 py-1 rounded-md mt-4 w-28">
-                <p>Tech</p>
-                <p className="text-red-700rounded-full cursor-pointer p-1">
-                  <RxCross2 />
-                </p>
-              </div>
-              <div className="flex justify-center items-center space-x-3 mr-3 bg-sky-600 px-2 py-1 rounded-md mt-4 w-28">
-                <p>Tech</p>
-                <p className="text-red-700rounded-full cursor-pointer p-1">
-                  <RxCross2 />
-                </p>
-              </div>
+              {categories?.map((c, k) => (
+                <div
+                  key={k}
+                  className="flex justify-center items-center space-x-3 mr-3 bg-sky-600 px-2 py-1 rounded-md mt-4 w-28"
+                >
+                  <p className="text-white">Tech</p>
+                  <p
+                    onClick={deleteCategory}
+                    className="text-white rounded-full cursor-pointer p-1"
+                  >
+                    <RxCross2 />
+                  </p>
+                </div>
+              ))}
             </div>
           </div>
+          <textarea
+            rows={12}
+            cols={30}
+            className="px-4 py-2 bg-transparent border rounded-lg"
+            placeholder="Enter Post Description"
+          />
+          <button className="w-full px-2 py-2 text-lg font-bold text-center min-w-[8rem] text-white transition-all rounded-md shadow-xl sm:w-auto bg-gradient-to-r from-blue-600 to-blue-500 hover:bg-gradient-to-b dark:shadow-blue-900 shadow-blue-200 hover:shadow-2xl hover:shadow-blue-400 hover:-tranneutral-y-px">
+            Create
+          </button>
         </form>
       </div>
       <Footer />
