b341a45aabe9912d4238dee4baabc64ae113df48
aliwert <154356044+aliwert@users.noreply.github.com>
2024-08-26 22:31:39 +0300

    feat: models & validations & custom error

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
+"use strict";
+
 /* ------------------------------------------------------- */
+
+require("dotenv").config();
 
 const express = require("express");
 const app = express();
-require("dotenv").config();
 
 // envVariables to process.env:
 const PORT = process.env?.PORT || 5000;
@@ -11,6 +14,9 @@ const HOST = process.env?.HOST || "127.0.0.1";
 //connect to the database
 const { dbConnection } = require("./src/configs/dbConnection.js");
 dbConnection();
+
+// Accept JSON:
+app.use(express.json());
 
 // RUN SERVER:
 app.listen(PORT, () => console.log(`http://${HOST}:${PORT}`));
