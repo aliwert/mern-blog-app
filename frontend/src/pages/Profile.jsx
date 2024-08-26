bcaa659e1c7ec3a5707d1bd703550dfbad2406cb
aliwert <154356044+aliwert@users.noreply.github.com>
2024-08-26 21:33:15 +0300

    feat: profile

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -10,25 +10,30 @@ const Profile = () => {
         <aside className="flex flex-col md:w-[70%] w-full">
           <h1 className="text-2xl font-bold text-sky-500 mb-4">Your Posts</h1>
           <ProfilePost />
+          <ProfilePost />
+          <ProfilePost />
         </aside>
 
-        <aside className="flex flex-col space-y-4 md:w[35%] w-full">
-          <h1 className="text-2xl font-bold text-sky-500 mb-4">Profile</h1>
-          <input
-            type="text"
-            className="bg-transparent border p-3 rounded-lg outline-none px-4 py-2"
-            placeholder="Username"
-          />
-          <input
-            type="email"
-            className="bg-transparent border p-3 rounded-lg outline-none px-4 py-2"
-            placeholder="Email"
-          />
-          <input
-            type="Password"
-            className="bg-transparent border p-3 rounded-lg outline-none px-4 py-2"
-            placeholder="Password"
-          />
+        <aside className=" md:w[35%] w-full md:items-end">
+          <div className="flex flex-col space-y-4 ">
+            <h1 className="text-2xl font-bold text-sky-500 mb-4">Profile</h1>
+            <input
+              type="text"
+              className="bg-transparent border p-3 rounded-lg outline-none px-4 py-2"
+              placeholder="Username"
+            />
+            <input
+              type="email"
+              className="bg-transparent border p-3 rounded-lg outline-none px-4 py-2"
+              placeholder="Email"
+            />
+            <input
+              type="Password"
+              className="bg-transparent border p-3 rounded-lg outline-none px-4 py-2"
+              placeholder="Password"
+            />
+          </div>
+
           <div className="flex items-center space-x-4 mt-7">
             <button className="bg-slate-600 text-white px-3 py-2 font-bold cursor-pointer transition-all rounded-md shadow-xl sm:w-auto bg-gradient-to-r from-blue-600 to-blue-500 hover:bg-gradient-to-b dark:shadow-blue-900 shadow-blue-200 hover:shadow-2xl hover:shadow-blue-400 hover:-translate-y-px">
               Update
