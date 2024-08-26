1fc5fe2cb86b000a6341a6f80230867617911bc3
aliwert <154356044+aliwert@users.noreply.github.com>
2024-08-26 22:43:42 +0300

    feat: user model

---
+++ backend/src/models/User.js
@@ -0,0 +1,28 @@
+"use strict";
+const { mongoose } = require("../configs/dbConnection.js");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", UserSchema);
