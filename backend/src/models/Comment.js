448fcfee96e61e8a973574b135a092008724fe1e
aliwert <154356044+aliwert@users.noreply.github.com>
2024-08-26 22:48:02 +0300

    feat: comment model

---
+++ backend/src/models/Comment.js
@@ -0,0 +1,30 @@
+"use strict";
+const { mongoose } = require("../configs/dbConnection.js");
+
+const CommentSchema = new mongoose.Schema(
+  {
+    comment: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    author: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    postId: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Comment", CommentSchema);
