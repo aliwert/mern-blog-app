797e020ad6fb117e7e52a695f1662fb423ebb8b3
aliwert <154356044+aliwert@users.noreply.github.com>
2024-08-26 22:46:22 +0300

    feat: post model

---
+++ backend/src/models/Post.js
@@ -0,0 +1,39 @@
+"use strict";
+const { mongoose } = require("../configs/dbConnection.js");
+
+const PostSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    photo: {
+      type: String,
+      required: false,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: Array,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Post", PostSchema);
