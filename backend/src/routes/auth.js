4304dae727476c6ac45f77603384a7653e0f0e8d
aliwert <154356044+aliwert@users.noreply.github.com>
2024-08-27 14:15:10 +0300

    fix: encrypt password

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -10,11 +10,41 @@ const jwt = require("jsonwebtoken");
 router.post("/signup", async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    const crypt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hashSync(password, crypt);
-    const newUser = new User({ username, email, password });
+
+    // Generate salt and hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create a new user with the hashed password
+    const newUser = new User({ username, email, password: hashedPassword });
     const savedUser = await newUser.save();
+
     res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+/* --------------------------------------------------------------*/
+
+/* --------------------------------------------------------------*/
+//Login
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    const validPassword = await bcrypt.compareSynce(password, user.password);
+    if (!validPassword) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ token });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
