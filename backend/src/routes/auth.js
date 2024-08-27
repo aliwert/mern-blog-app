0ac8807ce2869d9e282a113efbae0698bf43c26a
aliwert <154356044+aliwert@users.noreply.github.com>
2024-08-27 14:18:48 +0300

    feat: login

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -37,14 +37,11 @@ router.post("/login", async (req, res) => {
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
-    const validPassword = await bcrypt.compareSynce(password, user.password);
+    const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
       return res.status(401).json({ error: "Invalid credentials" });
     }
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-    res.status(200).json({ token });
+    res.status(200).json({ user });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
