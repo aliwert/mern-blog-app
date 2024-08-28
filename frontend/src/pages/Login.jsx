b7567a75abaa270ec567ee46c5921a9cd767aa39
aliwert <154356044+aliwert@users.noreply.github.com>
2024-08-28 18:21:39 +0300

    feat: add login functionality for successful site access

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,26 @@
-import { Link } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { URL } from "../url";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(URL + "/api/auth/login", {
+        email,
+        password,
+      });
+      navigate("/");
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="flex items-center justify-between px-5 md:px- [150px] py-3">
@@ -17,18 +37,28 @@ const Login = () => {
             Welcome to Blog App Please Login To Your Account
           </h1>
           <input
+            onChange={(e) => setEmail(e.target.value)}
             className="w-full px-3 py-2 border border-yellow-400"
             type="text"
             placeholder="Enter Your Email"
           />
           <input
+            onChange={(e) => setPassword(e.target.value)}
             className="w-full px-3 py-2 border border-yellow-400"
             type="password"
             placeholder="Enter Your Password"
           />
-          <button className="w-full px-2 py-2 text-lg font-bold text-center min-w-[8rem] text-white transition-all rounded-md shadow-xl sm:w-auto bg-gradient-to-r from-blue-600 to-blue-500 hover:bg-gradient-to-b dark:shadow-blue-900 shadow-blue-200 hover:shadow-2xl hover:shadow-blue-400 hover:-tranneutral-y-px">
+          <button
+            onClick={handleLogin}
+            className="w-full px-2 py-2 text-lg font-bold text-center min-w-[8rem] text-white transition-all rounded-md shadow-xl sm:w-auto bg-gradient-to-r from-blue-600 to-blue-500 hover:bg-gradient-to-b dark:shadow-blue-900 shadow-blue-200 hover:shadow-2xl hover:shadow-blue-400 hover:-tranneutral-y-px"
+          >
             Log In
           </button>
+          {error && (
+            <span className="text-red-500 text-lg">
+              Your Email or Password wrong!
+            </span>
+          )}
           <div className="flex justify-center items-center space-x-3">
             <p>Don't have an account?</p>
             <p className="text-sky-500 hover:text-sky-950">
