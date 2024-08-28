75b2a7278aca0df12016ef79af2eab144359c2d6
aliwert <154356044+aliwert@users.noreply.github.com>
2024-08-28 16:51:28 +0300

    feat: hamburger navbar for small screens

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -4,22 +4,34 @@ const Menu = () => {
   return (
     <div className="bg-sky-500 w-[200px] flex flex-col items-start absolute top-10 right-10 rounded-lg p-5 space-y-5">
       {!user && (
-        <h3 className="text-white text-lg hover:text-sky-900 ">Login</h3>
+        <h3 className="text-white text-lg hover:text-sky-900 cursor-pointer">
+          Login
+        </h3>
       )}
       {!user && (
-        <h3 className="text-white text-lg hover:text-sky-900 ">Sign Up</h3>
+        <h3 className="text-white text-lg hover:text-sky-900 cursor-pointer">
+          Sign Up
+        </h3>
       )}
       {user && (
-        <h3 className="text-white text-lg hover:text-sky-900 ">Profile</h3>
+        <h3 className="text-white text-lg hover:text-sky-900 cursor-pointer">
+          Profile
+        </h3>
       )}
       {user && (
-        <h3 className="text-white text-lg hover:text-sky-900 ">Create Post</h3>
+        <h3 className="text-white text-lg hover:text-sky-900 cursor-pointer">
+          Create Post
+        </h3>
       )}
       {user && (
-        <h3 className="text-white text-lg hover:text-sky-900 ">My Blogs</h3>
+        <h3 className="text-white text-lg hover:text-sky-900 cursor-pointer">
+          My Blogs
+        </h3>
       )}
       {user && (
-        <h3 className="text-white text-lg hover:text-sky-900 ">Logout</h3>
+        <h3 className="text-white text-lg hover:text-sky-900 cursor-pointer">
+          Logout
+        </h3>
       )}
     </div>
   );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import { Link } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
+import { FaBarsStaggered } from "react-icons/fa6";
+import { useState } from "react";
+import Menu from "./Menu";
 
 const Navbar = () => {
-  const user = true;
+  const [menu, setMenu] = useState(false);
+  const showMenu = () => {
+    setMenu(!menu);
+  };
+  const user = false;
 
   return (
     <div className="flex items-center justify-between px-5 md:px- [150px] py-3">
@@ -19,10 +26,7 @@ const Navbar = () => {
           type="text"
         />
       </div>
-      <div className="flex items-center justify-center space-x-1 md:space-x-10">
-        <h2>
-          <Link></Link>
-        </h2>
+      <div className="hidden md:flex items-center justify-center space-x-1 md:space-x-10">
         {user ? (
           <h2 className="text-sky-500">
             <Link to="/createpost">Create A Post</Link>
@@ -39,6 +43,12 @@ const Navbar = () => {
             <Link to="/signup">Sign Up</Link>
           </h2>
         )}
+      </div>
+      <div onClick={showMenu} className="md:hidden text-2xl">
+        <p className="cursor-pointer">
+          <FaBarsStaggered />
+          {menu ? <Menu /> : null}
+        </p>
       </div>
     </div>
   );
