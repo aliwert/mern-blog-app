91337310ca757ca54de39323045a9ae810ceb139
aliwert <154356044+aliwert@users.noreply.github.com>
2024-08-28 18:46:30 +0300

    feat: refetch

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -63,4 +63,18 @@ router.get("/logout", (req, res) => {
   }
 });
 
+//refetch
+router.get("/refetch", (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({ error: "No token provided" });
+  }
+  jwt.verify(token, process.env.JWT_SECRET, {}, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: "Failed to authenticate token" });
+    }
+    res.status(200).json(user);
+  });
+});
+
 module.exports = router;
